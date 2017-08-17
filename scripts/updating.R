5f3756f1ea23fb4d0d8c45f696c362fce543f489
BroVic <victorordu@outlook.com>
2017-08-17 14:38:16 +0100

    Improves on the script significantly

--- scripts/updating.R
+++ scripts/updating.R
@@ -19,23 +19,17 @@
         ##                                       ##
         ###########################################
 
-# STEP 1: Preliminaries
-## Set up working directory
-newdir <- "R-installation"
-dir.create(newdir)
-setwd(newdir)
-
-# STEP 2: Load the 'installr' package
+# STEP 1: Load the 'installr' package
 if (!require(installr)) {
         install.packages(installr)
 }
 
-# STEP 3: Make a local list of your current packages
+# STEP 2: Make a local list of your current packages
 tmp <- installed.packages()
 installedPkgs <- as.vector(tmp[is.na(tmp[ , "Priority"]), 1])
 save(installedPkgs, file = "old-packages.rda")  # for use after installation
 
-# STEP 4: Update your R installation
+# STEP 3: Update your R installation
 installr::updateR(copy_packages = TRUE,
                   browse_news = FALSE,
                   install_R = TRUE,
@@ -48,29 +42,31 @@ installr::updateR(copy_packages = TRUE,
                   keep_install_file = TRUE,
                   silent = FALSE)
 
-# STEP 5: Reinstall your old packages (from this point, run in your
-# fresh R installation's workspace). To quickly open this script over
-# there, run file.edit("updating.R") in the console.
-# NOTE: This may take a while - grap some coffee!
+# STEP 4: Reinstall your old packages:
+# Inside the new R session, navigate to same folder and open this script.
+# Continue below the dotted line.
+#.......................................................................
 load("old-packages.rda")
 tmp <- installed.packages()
 newly_installed <- as.vector(tmp[is.na(tmp[ , "Priority"]), 1])
 missing <- setdiff(installedPkgs, newly_installed)
-message(paste(length(missing), "packages missing!"))
-install.packages(missing)
 
-# STEP 6 (OPTIONAL):
-## If you have LOTS of R scripts you are advised not to update your packages
+qty <- length(missing)
+if (qty != 0) {
+	message(paste(qty, "packages missing!"))
+	install.packages(missing)
+} else { message("No packages missing.") }
+
+# STEP 5 (OPTIONAL):
+## If you have LOTS of R packages you are advised not to update them
 ## until you have implemented Package Dependency Management with, say,
-## 'packrat', which is a topic for another day. If curious, visit
+## 'packrat', which is a topic for another day. For more info, visit
 ## http://rstudio.github.io/packrat/
 switch(menu(c("Yes", "No"),
             title = "Do you want to update your packages?"),
        update.packages(),
        cat("Packages not updated. End of R installation.\n"))
 
-# STEP 7: Clean-up
-## Removes the directory created for this installation and clears workapsce
+# STEP 6: Clean-up
 rm(list = ls())
-setwd("../")
-unlink("R-installation", recursive = TRUE)
+file.remove("old-packages.rda")
